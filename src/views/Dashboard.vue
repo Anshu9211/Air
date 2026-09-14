7070868f3db91ad755a16de4e64cf88948590566
Anshu9211 <anshulgarg1999@gmail.com>
2026-09-14 11:24:20 +0530

    watch

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="dashboard">
+    <h1>Dashboard</h1>
+    <p>Welcome to Airline Dashboard</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.dashboard {
+  padding: 40px;
+}
+
+h1 {
+  margin-bottom: 10px;
+}
+</style>
+
---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>hye</h1>
+    </div>
+</template>
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const email = ref("");
+const password = ref("");
+const error = ref("");
+
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+
+
+
+
+
